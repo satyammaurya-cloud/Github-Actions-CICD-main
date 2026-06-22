58dad13c608d95456b6e6a5a8056c742e93a98bf
Satyam <satyammaurya1001@gmail.com>
2026-06-22 16:35:52 +0530

    code provider update

--- main.tf
+++ main.tf
@@ -1,17 +1,14 @@
-resource "aws_vpc" "name" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-      Name = "hcl-vpccc-2"
-    }
-  
+resource "aws_vpc" "vpc1" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "hcl-vpc-1"
+  }
 }
 
-
-resource "aws_subnet" "name2" {
-    vpc_id = aws_vpc.name.id
-    cidr_block = "10.0.0.0/24"
-    tags = {
-      Name = "mysubnet"
-    }
-  
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.vpc1.id
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = "mysubnet"
+  }
 }
--- provider.tf
+++ provider.tf
@@ -1,12 +1,18 @@
-provider "aws" {
-  
-}
-
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = ">4.0" #morthan 4.0 version
+      source  = "hashicorp/aws"
+      version = "6.51.0"
     }
   }
+  backend "s3" {
+    bucket       = "aws-s3-backend-bucket-123445"
+    key          = "path/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
